1aef1988b24d04dc25fff2fad4491d62416a1ee4
rigma <rigbuntu@gmail.com>
2020-05-04 21:37:43 +0200

    feat: add sobel filter and applying it on input

--- main.go
+++ main.go
@@ -4,7 +4,26 @@ import (
 	"gocv.io/x/gocv"
 
 	"fmt"
+	"image"
 )
+
+func sobelFilter(input *gocv.Mat) (output gocv.Mat) {
+	blured_input := gocv.NewMat()
+	defer blured_input.Close()
+
+	gocv.GaussianBlur(*input, &blured_input, image.Pt(3, 3), 0, 0, gocv.BorderDefault)
+
+	grad_x, grad_y := gocv.NewMat(), gocv.NewMat()
+	defer grad_x.Close()
+	defer grad_y.Close()
+
+	gocv.Sobel(blured_input, &grad_x, -1, 1, 0, 3, 1.0, 0.0, gocv.BorderDefault)
+	gocv.Sobel(blured_input, &grad_y, -1, 0, 1, 3, 1.0, 0.0, gocv.BorderDefault)
+
+	output = gocv.NewMat()
+	gocv.AddWeighted(grad_x, 0.5, grad_y, 0.5, 0, &output)
+	return
+}
 
 func main() {
 	fmt.Println("Launching GoView...")
@@ -20,9 +39,13 @@ func main() {
 	input := gocv.NewMat()
 	defer input.Close()
 
+	output := gocv.NewMat()
+	defer output.Close()
+
 	for {
 		webcam.Read(&input)
-		window.IMShow(input)
+		output = sobelFilter(&input)
+		window.IMShow(output)
 
 		if window.WaitKey(1) >= 0 {
 			break
