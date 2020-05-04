df13ee3843767ca18031555ffb63477a2635b6a6
rigma <rigbuntu@gmail.com>
2020-05-04 21:03:51 +0200

    feat: add initial code to retrieve video device

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"gocv.io/x/gocv"
+
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	fmt.Println("Launching GoView...")
+
+	webcam, err := gocv.VideoCaptureDevice(0)
+	if err != nil {
+		panic("Unable to retrieve the video capture device!")
+	}
+
+	window := gocv.NewWindow("GoView")
+	defer window.Close()
+
+	input := gocv.NewMat()
+	defer input.Close()
+
+	for {
+		webcam.Read(&input)
+		window.IMShow(input)
+
+		if window.WaitKey(1) >= 0 {
+			break
+		}
+	}
 }
